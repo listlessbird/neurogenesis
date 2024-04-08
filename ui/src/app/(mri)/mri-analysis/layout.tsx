7f87f277f4534b0270968aed2e8fe6612429df4b
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-04-08 08:26:50 +0530

    feat(ui): set the sidebar fixed

--- ui/src/app/(mri)/mri-analysis/layout.tsx
+++ ui/src/app/(mri)/mri-analysis/layout.tsx
@@ -2,7 +2,7 @@ import { AnalysisStoreProvider } from "./analysis-provider"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="size-full ">
+    <div className="size-full">
       <main className="h-full p-2">
         <AnalysisStoreProvider>{children}</AnalysisStoreProvider>
       </main>
